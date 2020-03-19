665461050ba99ea14e09b45c01a1a8c42b966fa9
Kayon-Marie <k.marie.douglas@hotmail.com>
2020-03-19 11:56:03 -0500

    added add client functions

---
+++ src/add.js
@@ -0,0 +1,62 @@
+'use strict';
+
+function saveData(data){
+    // clientData(data);
+    // var idNum = generateID();
+    // var client = updateDataObject(idNum, data);
+}
+
+function clientData(data){
+    // return validateClient(data) && formatDOB(data.dob);
+}
+
+function saveData(data){
+
+}
+
+function validateClient(cData){
+    // var fname = cData.fname;
+    // var lname = cData.lname;
+    // var address = cData.address;
+    // var telenum = cData.telenum;
+    // var email = cData.email
+    // return validateName(fname) && validateName(lname) && validateAddress(address) && validateEmail(email) && validateTelephone(telenum)
+}
+
+function validateName(name){
+    // return typeof name == 'string'
+}
+
+function validateAddress(address){
+    // return true
+}
+
+function validateEmail(email){
+    // return true
+}
+
+function validateTelephone(telenum){
+    // return true
+}
+
+function formatDOB(dob){
+    // var regEx = /^\d{4}-\d{2}-\d{2}$/;
+    // if(!dob.match(regEx)) return false;  // Invalid format
+    // var d = new Date(dob);
+    // var dNum = d.getTime();
+    // if(!dNum && dNum !== 0) return false; // NaN value, Invalid date
+    // return d.toISOString().slice(0,10) === dob;
+    // return true
+}
+
+function generateID(){
+    // return "1234"
+}
+
+function updateDataObject(idNum, data){
+    // return data
+}
+
+function validateZipCode(code){
+    // return true
+}
